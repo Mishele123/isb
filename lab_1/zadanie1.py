4626bac0d698469c9f9057d22eb64be3b4fdbd9d
Mishele123 <misha228665161@gmail.com>
2024-04-02 20:37:42 +0400

    using argparse

--- lab_1/zadanie1.py
+++ lab_1/zadanie1.py
@@ -1,13 +1,13 @@
 
 import os
 from work_with_files import *
+import argparse
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def encyption(path_to_text: str, path_to_json: str, path_to_result_file) -> None:
     """encrypt the proposed text"""
-
     step = int(read_json(path_to_json)["step"])
     text = read_file(path_to_text).upper()
     output = ""
@@ -22,11 +22,9 @@ def encyption(path_to_text: str, path_to_json: str, path_to_result_file) -> None
 
 def decription(path_to_text: str, path_to_json: str, path_to_result_file) -> None:
     """decription the proposed text"""
-
     step = int(read_json(path_to_json)["step"])
     text = read_file(path_to_text).upper()
     output = ""
-
     for i in text:
         if i in ALPHABET:
             new_position = ALPHABET.find(i) - step # get new position
@@ -37,5 +35,13 @@ def decription(path_to_text: str, path_to_json: str, path_to_result_file) -> Non
     write_file(path_to_result_file, output)
 
 
-encyption("D:\\allLabs\\isb\\lab_1\\original_text1.txt", "D:\\allLabs\\isb\\lab_1\\encryption_key.json", "D:\\allLabs\\isb\lab_1\\encrypted1.txt")
-decription("D:\\allLabs\\isb\lab_1\\encrypted1.txt", "D:\\allLabs\\isb\\lab_1\\encryption_key.json", "D:\\allLabs\\isb\\lab_1\\decrypted.1txt")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Substitution encryption/decryption")
+    parser.add_argument("encryption_key", help="Path to the encryption key file")
+    parser.add_argument("original_text", help="Path to the original text")
+    parser.add_argument("encrypted_file", help="Path to the encrypted file")
+    parser.add_argument("decrypted_file", help="Path to the decrypted file")
+    args = parser.parse_args()
+
+    encyption(args.original_text, args.encryption_key, args.encrypted_file)
+    decription(args.encrypted_file, args.encryption_key, args.decrypted_file)
