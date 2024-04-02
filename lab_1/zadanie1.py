e1ef747b32fcc9ec90981e15bf2441eca3026527
Mishele123 <misha228665161@gmail.com>
2024-04-02 20:21:05 +0400

    function decription

--- lab_1/zadanie1.py
+++ lab_1/zadanie1.py
@@ -5,14 +5,12 @@ from work_with_files import *
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-def encyption(path_to_text: str, path_to_json: str) -> None:
+def encyption(path_to_text: str, path_to_json: str, path_to_result_file) -> None:
     """encrypt the proposed text"""
 
     step = int(read_json(path_to_json)["step"])
     text = read_file(path_to_text).upper()
     output = ""
-
-    print(text)
 
     for i in text:
         if i in ALPHABET:
@@ -20,7 +18,22 @@ def encyption(path_to_text: str, path_to_json: str) -> None:
             output += ALPHABET[new_position]
         else:
             output += i
+    write_file(path_to_result_file, output)
 
-    print(output)
+def decription(path_to_text: str, path_to_json: str, path_to_result_file) -> None:
+    """decrypt the proposed text"""
 
-encyption("D:\\allLabs\\isb\\lab_1\\original_text1.txt", "D:\\allLabs\\isb\\lab_1\\encryption_key.json")
+    step = int(read_json(path_to_json)["step"])
+    text = read_file(path_to_text).upper()
+    output = ""
+
+    for i in text:
+        if i in ALPHABET:
+            new_position = ALPHABET.find(i) - step # get new position
+            output += ALPHABET[new_position]
+        else:
+            output += i
+    write_file(path_to_result_file, output)
+
+
+encyption("D:\\allLabs\\isb\\lab_1\\original_text1.txt", "D:\\allLabs\\isb\\lab_1\\encryption_key.json", "D:\\allLabs\\isb\lab_1\\encrypted1.txt")
