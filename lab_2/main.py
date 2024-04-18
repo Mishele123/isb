b19127f2d681c1a99ad68920172d6b2dfd7ecde8
Mishele123 <misha228665161@gmail.com>
2024-04-18 19:25:54 +0400

    refinement of functions

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,11 +1,11 @@
 import json
 import argparse
 import math
-import re
-import scipy
+
+from typing import Tuple
 
 
-def work_with_json_file(path: str) -> tuple:
+def work_with_json_file(path: str) -> Tuple[str, str, str]:
     """Function get c++ \ java secuences \ path to result file from json file
     parameters:
     path: path to json file
@@ -13,7 +13,7 @@ def work_with_json_file(path: str) -> tuple:
     try:
         with open(path, "r", encoding="utf-8") as file:
             json_data = json.load(file)
-            return (json_data["c++"], json_data["java"], json_data["path_ro_result"])
+            return (json_data["c++"], json_data["java"], json_data["path_to_result_file"])
     except Exception as ex:
         print(f"Error with file: {str(ex)}")
 
@@ -28,14 +28,13 @@ def frequency_bit_test(bits_sequence: str) -> float:
     try:
         N = len(bits_sequence)
         S = 0
-
         for i in bits_sequence:
-            if i == 1:
+            if i == "1":
                 x = 1
             else:
                 x = -1
             S += 1 / math.sqrt(N) * x
-        P = math.erfc(s / math.sqrt(2))
+        P = math.erfc(S / math.sqrt(2))
         return P
     except Exception as ex:
         print(f"Error : {str(ex)}")
@@ -43,11 +42,12 @@ def frequency_bit_test(bits_sequence: str) -> float:
 
 if __name__ == "__main__":
     try:
-        parser = argparse.ArgumentParser(description="Argument parser")
-        parser.add_argument("random_bits_json", help="Path to json file with sequences")
+        parser = argparse.ArgumentParser(description="Parse Arguments")
+        parser.add_argument("random_bits_json", help="Path to the json file")
         args = parser.parse_args()
-
+        print(args.random_bits_json)
         cpp_row, java_row, path_ro_res_file = work_with_json_file(args.random_bits_json)
-
+        print(cpp_row)
+        print(frequency_bit_test(cpp_row))
     except Exception as ex:
         print(f"Error: {str(ex)}")
