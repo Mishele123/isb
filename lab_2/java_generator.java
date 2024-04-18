8d9767a73e6c9044e3f2c2e77d7d75fe3a6449e3
Mishele123 <misha228665161@gmail.com>
2024-04-18 18:15:03 +0400

    first commit

---
+++ lab_2/java_generator.java
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+
+int main() 
+{
+    srand(time(nullptr));
+    const int length = 128;
+    for (int i = 0; i < length; ++i) 
+    {
+        int random_bit = rand() % 2; 
+        std::cout << random_bit;
+    }
+    return 0;
+}
