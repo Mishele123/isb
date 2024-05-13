49b6f14a9b9d4846f67476cfaab5efbd3100f791
Mishele123 <misha228665161@gmail.com>
2024-05-13 17:05:09 +0400

    encrypt data function

---
+++ lab_3/symmetric_crypto.py
@@ -0,0 +1,22 @@
+import os
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
+
+
+def encrypt_data(symmetric_key, text):
+    """
+    Encrypt text using Chacha20 symmetric ecryption algorithm
+    Args:
+        symmetric_key (bytes) : symmetric key for encryption
+        text (bytes) : text for encrypted
+    Return:
+        result encrypted text (bytes) 
+    """
+    try:
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.ChaCha20(symmetric_key, iv), mode=None)
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(text) + encryptor.finalize()
+        return iv + ciphertext
+    except Exception as ex:
+        print(f"Error: {ex}")
