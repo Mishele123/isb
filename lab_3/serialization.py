1c30beb6b53b1c3b582d17be194964fe08c6771a
Mishele123 <misha228665161@gmail.com>
2024-05-13 19:42:24 +0400

    all serialization.py

--- lab_3/serialization.py
+++ lab_3/serialization.py
@@ -19,3 +19,31 @@ def serialize_private_key(rsa_key):
     except Exception as ex:
         print(f"Error: {ex}")
 
+def serialize_public_key(public_key):
+    """serialize public key to PEM format
+    args:
+        public_key (bytes) : public key to serialize
+    return:
+        serialized public key (bytes) 
+    """
+    try:
+        return public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+    except Exception as ex:
+        print(f"Error: {ex}")
+
+
+def load_private_key(private_key_bytes):
+    """
+    load private key from PEM file
+    args:
+        private_key_bytes (bytes): PEM file
+    return:
+        private key (bytes)
+    """
+    try:
+        return serialization.load_pem_private_key(private_key_bytes, password=None, backend=default_backend())
+    except Exception as ex:
+        print(f"Error: {ex}")
